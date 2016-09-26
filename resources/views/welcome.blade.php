6426c3b706e5df0c62ca8ee641185a862f3c79c1
janulius <janu.lius021@gmail.com>
2016-09-26 15:28:26 +0700

    ping

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,3 +2,4 @@ Hello Yose
 
 <a id="repository-link" href="#readme">YoseTheGame</a>
 <a href="https://uk.linkedin.com/in/davidthomaskelly" id="contact-me-link">Contact me</a>
+<a href="https://powerrangers.herokuapp.com/ping" id="ping-challenge-link">Ping Challenge</a>
