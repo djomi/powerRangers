9e43549f4e8be44ad48a80f84bda0f210484c428
janulius <janu.lius021@gmail.com>
2016-09-26 15:23:36 +0700

    add href

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,4 @@
 Hello Yose
 
 <a id="repository-link" href="#readme">YoseTheGame</a>
-<a href="" id="contact-me-link">Contact me</a>
+<a href="https://uk.linkedin.com/in/davidthomaskelly" id="contact-me-link">Contact me</a>
