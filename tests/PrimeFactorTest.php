02be5f7bf5714eb3dd5ca5617db59c645b425007
lutfi <mlutfi@gmail.com>
2016-09-26 17:03:42 +0700

    fix tdd

--- tests/PrimeFactorTest.php
+++ tests/PrimeFactorTest.php
@@ -9,25 +9,25 @@ class PrimeFactorTest extends TestCase
      */
     public function testPowOf2Number()
     {
-        $this->call('GET', 'primeFactors', [ 'numbers' => 32 ]);
+        $this->call('GET', 'primeFactors', [ 'number' => 32 ]);
         $this->seeJson([ 'number' => 32, 'decomposition' => [ 2, 2, 2, 2, 2 ] ]);
     }
 
     public function testNotNumber()
     {
-        $this->call('GET', 'primeFactors', [ 'numbers' => 'hello' ]);
+        $this->call('GET', 'primeFactors', [ 'number' => 'hello' ]);
         $this->seeJson([ 'number' => 'hello', 'error' => 'not a number' ]);
     }
 
     public function testNumber()
     {
-        $this->call('GET', 'primeFactors', [ 'numbers' => 300 ]);
+        $this->call('GET', 'primeFactors', [ 'number' => 300 ]);
         $this->seeJson([ 'number' => 300, 'decomposition' => [ 2, 2, 3, 5, 5 ] ]);
     }
 
     public function testBiggerThan1e6()
     {
-        $this->call('GET', 'primeFactors', [ 'numbers' => 1000001 ]);
+        $this->call('GET', 'primeFactors', [ 'number' => 1000001 ]);
         $this->seeJson([ 'number' => 1000001, 'error' => 'too big number (>1e6)' ]);
     }
 }
