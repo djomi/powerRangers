0aa0e73a2842c0e9643a06bc39c3b92ee8ae2846
janulius <janu.lius021@gmail.com>
2016-09-27 14:52:20 +0700

    feedback k6

--- resources/views/astroport.blade.php
+++ resources/views/astroport.blade.php
@@ -27,7 +27,7 @@
 	</div>
 </div>
 <form>
-	<input name="ship" id="ship" class="ship" value="Millenium Falcon">
+	<input name="ship" id="ship" class="ship" value="">
 	<button id="dock" class="dock" type="submit">dock</button>
 </form>
 
