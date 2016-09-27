e920d8fa225402438cd144ad5dfa6794d8d04dbf
lutfi <mlutfi@gmail.com>
2016-09-27 11:21:53 +0700

    minesweeper data injection2

--- resources/views/minesweeper.blade.php
+++ resources/views/minesweeper.blade.php
@@ -4,14 +4,13 @@
          src = "https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 		
       <script type = "text/javascript" language = "javascript">
-         
-      	function mine(id) {
-    		 $(document).ready(function() {
-	            $(id).click(function(event){
-	               $(id).load('../lost.html');
-	            });
-	         });
-		}
+         $( document ).ready(function() {
+			$("#cell-3x6").click(function(){
+			    $("#cell-3x6").load("../lost.html");
+			});
+		});
+      	
+    		
 
         
 </script>
@@ -21,7 +20,7 @@
 	<tr>
 	@for($a = 1; $a <= 8; $a++)
 		@if($i==3 && $a== 6)
-			<td onclick="mine('#cell-{{ $i.'x'.$a }}')" id='cell-{{ $i.'x'.$a }}'>bomb</td>
+			<td id='cell-{{ $i.'x'.$a }}'>bomb</td>
 		@else
 			<td id='cell-{{ $i.'x'.$a }}'>empty</td>
 		@endif
