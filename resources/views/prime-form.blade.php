5def224e953f1ff0d60c87064f44cefcd031010f
rheniere <davidlienardy1@gmail.com>
2016-09-27 11:05:43 +0700

    add new element

--- resources/views/prime-form.blade.php
+++ resources/views/prime-form.blade.php
@@ -1,7 +1,6 @@
-<head>
-    <title>Invitation</title>
-</head>
 <form action="" id="title invitation" title="invitation">
+    <h1 id="title">Title</h1>
+    <h1 id="invitation">Invitation</h1>
     <input type="text" id="number">
     <button id="go">Submit</button>
 </form>
