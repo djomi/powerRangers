8624ce0fff0c6903c3301438bb5619a114914329
janulius <janu.lius021@gmail.com>
2016-09-27 16:08:54 +0700

    prime factory

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,3 +3,5 @@ Hello Yose
 <a id="repository-link" href="https://github.com/djomi/powerRangers#readme">YoseTheGame</a> 
 <a href="https://powerrangers.herokuapp.com/ping" id="contact-me-link">Contact Me</a>
 <a href="https://powerrangers.herokuapp.com/ping" id="ping-challenge-link">Ping Challenge</a>
+
+<a href="https://powerrangers.herokuapp.com/primeFactors/ui" id="prime-factors-decomposition-link">Prime Factors-decomposition-link</a>
